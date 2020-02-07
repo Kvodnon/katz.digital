7e7c0e55203e34196637cae077bec694775a6329
Kirill Kiselev <skulpin@mail.ru>
2020-02-07 18:27:14 +0700

    добавлен скрипт и паралакс эффект на первый экран

---
+++ js/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let whoWrapper = document.querySelector('.who__wrapper'),
+    logoSection = document.querySelector('.logo__section');
+
+function toSecondScreen() {
+  if (logoSection.offsetHeight < window.scrollY) {
+    // console.log('this.scrollY: ', window.scrollY);
+    window.removeEventListener('scroll', toSecondScreen);
+
+    whoWrapper.classList.remove('fixed');
+  }
+}
+
+window.addEventListener('scroll', toSecondScreen);
