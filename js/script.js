bfa6b5e0db565869d435ab82af2dfa0264108254
Kirill Kiselev <skulpin@mail.ru>
2020-02-19 01:27:39 +0700

    changes

--- js/script.js
+++ js/script.js
@@ -1,28 +1,34 @@
-'use strict';
+(function() {
+  'use strict';
 
-const whoWrapper = document.querySelector('.who__wrapper'),
-    whoSection = document.querySelector('.who__section'),
-    letWrapper = document.querySelector('.let__wrapper'),
-    letSection = document.querySelector('.let__section'),
-    servicesWrapper = document.querySelector('.services-wrapper'),
-    servicesSection = document.querySelector('.services__section');
+  const whoWrapper = document.querySelector('.who__wrapper'),
+      whoSection = document.querySelector('.who__section'),
+      letWrapper = document.querySelector('.let__wrapper'),
+      letSection = document.querySelector('.let__section'),
+      servicesWrapper = document.querySelector('.services-wrapper'),
+      servicesSection = document.querySelector('.services__section');
 
-function toSecondScreen() {
-  if (whoSection.offsetTop < window.scrollY) {
-    whoWrapper.classList.remove('fixed');
+  if (window.screen.width > 768) {
+    servicesWrapper.classList.add('fixed');
   }
 
-  if (letSection.offsetTop < window.scrollY) {
-    letWrapper.classList.remove('fixed');
+  function toSecondScreen() {
+    if (whoSection.offsetTop < window.scrollY) {
+      whoWrapper.classList.remove('fixed');
+    }
+
+    if (letSection.offsetTop < window.scrollY) {
+      letWrapper.classList.remove('fixed');
+    }
+
+    if (servicesSection.offsetTop < window.scrollY && window.screen.width > 768) {
+      window.removeEventListener('scroll', toSecondScreen);
+
+      servicesWrapper.classList.remove('fixed');
+    }
   }
 
-  if (servicesSection.offsetTop < window.scrollY) {
-    window.removeEventListener('scroll', toSecondScreen);
+  window.addEventListener('scroll', toSecondScreen);
 
-    servicesWrapper.classList.remove('fixed');
-  }
-}
-
-window.addEventListener('scroll', toSecondScreen);
-
-VanillaTilt.init(document.querySelectorAll(".js-tilt"));
+  VanillaTilt.init(document.querySelectorAll(".js-tilt"));
+})();
