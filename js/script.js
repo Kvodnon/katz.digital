29ef3f1f6a536d90839850dc351f26b111b6709c
Kirill Kiselev <skulpin@mail.ru>
2020-02-18 23:59:31 +0700

    changes

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@
 const whoWrapper = document.querySelector('.who__wrapper'),
     whoSection = document.querySelector('.who__section'),
     letWrapper = document.querySelector('.let__wrapper'),
-    letSection = document.querySelector('.let__section');
+    letSection = document.querySelector('.let__section'),
+    servicesWrapper = document.querySelector('.services-wrapper'),
+    servicesSection = document.querySelector('.services__section');
 
 function toSecondScreen() {
   if (whoSection.offsetTop < window.scrollY) {
@@ -11,9 +13,13 @@ function toSecondScreen() {
   }
 
   if (letSection.offsetTop < window.scrollY) {
+    letWrapper.classList.remove('fixed');
+  }
+
+  if (servicesSection.offsetTop < window.scrollY) {
     window.removeEventListener('scroll', toSecondScreen);
 
-    letWrapper.classList.remove('fixed');
+    servicesWrapper.classList.remove('fixed');
   }
 }
 
